1d441aee3b475c8a308e2e0dec8328032c8d55e2
Kevin Schaefer <kevin.l.schaefer1394@gmail.com>
2026-01-10 14:33:49 -0600

    another fix

--- ui/pantry-ui/src/app/components/hardware/hardware-list/hardware-list.component.ts
+++ ui/pantry-ui/src/app/components/hardware/hardware-list/hardware-list.component.ts
@@ -32,7 +32,7 @@ export class HardwareListComponent implements OnInit {
     }
 
     printTestLabel(device: any) {
-        if (device.type === 'PRINTER' && device.status === 'ONLINE') {
+        if (device.type === 'PRINTER' && (device.status === 'ONLINE' || device.status === 'READY')) {
             this.labelService.printQuickLabel('TEST').subscribe({
                 next: (res) => {
                     this.snackBar.open('Test label sent to printer', 'Close', { duration: 3000 });
